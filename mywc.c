cd6513293c1a713e6ed4cc761cf31750a17f6b95
100471976 <100471976@alumnos.uc3m.es>
2023-03-07 14:56:21 +0100

    primera función

--- mywc.c
+++ mywc.c
@@ -1,8 +1,15 @@
 //P1-SSOO-22/23
+/*El programa mywc abrirá un fichero especificado como argumento, contará el
+número de lı́neas, palabras y bytes del mismo, y mostrará estas por la salida estándar (la
+consola) utilizando las llamadas al sistema que considere oportunas.*/
 
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 
 
 int main(int argc, char *argv[])
@@ -14,18 +21,28 @@ int main(int argc, char *argv[])
 		printf("Too few arguments\n");
 		return -1;
 	}
+
+	// Abrimos el fichero pasado como parámetro
 	int fd;
 	fd = open(argv[1], O_RDONLY);
 
 	if (fd < 0){
-		printf("ERROR DE LECTURA \n");
+		perror("ERROR DE APERTURA");
 		exit(-1);
 	}
 
-	int buf;
+	// Leemos el contenido del fichero byte a byte
+
+	char buf[1];
 	int bytes;
-	bytes = read(argv[1], buf, 4);
-
-	printf(bytes);
-
+	
+	while ((bytes = read(fd, buf, 1)) > 0){ // Un char son 4 bytes
+		printf("Read: %c \n", buf[0]);
+	}
+	
+	if (bytes < 0){
+		perror("ERROR DE LECTURA");
+		exit(-1);
+	}
+	
 }
